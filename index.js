b1d0dc7c9717f8bf6abd6a9e2e03fb9758fdea61
toskan <tamar.oskanian@gmail.com>
2021-09-09 12:28:25 -0400

    changed greeting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 //create route handler and associate with a given route
 app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
+	res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
