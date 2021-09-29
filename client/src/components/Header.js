75cbaae61f624b3fd2885783a6d3c2192e8d9e4c
toskan <tamar.oskanian@gmail.com>
2021-09-29 15:45:27 -0400

    stripe keys added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">Login With Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a href="/api/logout">Logout</a>
+					</li>
+				);
+		}
+	}
+
 	render() {
 		return (
 			<nav className="nav-wrapper">
-				<a href="" className="left brand-logo">
+				<Link
+					to={this.props.auth ? '/surveys' : '/'}
+					className="left brand-logo"
+				>
 					Emaily
-				</a>
-				<ul className="right">
-					<li>
-						<a href="">Login With Google</a>
-					</li>
-				</ul>
+				</Link>
+				<ul className="right">{this.renderContent()}</ul>
 			</nav>
 		);
 	}
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
